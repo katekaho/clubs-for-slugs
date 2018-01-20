a5bc9230bcb5e729d1b33d27b197c14cbf91c8cf
Kate Miller <kat3miller@gmail.com>
2018-01-20 13:31:53 -0800

    Add filter to clubs and change button names

--- orgs.js
+++ orgs.js
@@ -1,4 +1,4 @@
-const container = document.getElementById('page-content');
+const container = document.getElementById('orgs-list');
 
 var clubs = [
       { name: 'Chemistry Club', category: 'academic',
@@ -37,7 +37,7 @@ clubs = clubs.sort(function(a, b) {
 // Creates div with name of each club
 for (var i = 0; i < clubs.length; i++) {
       var div = document.createElement("div");
-      div.classList.add("org");
+      div.classList.add("filterDiv");
       div.classList.add(clubs[i].category);
       var header = document.createElement("h2");
       var node1 = document.createTextNode(clubs[i].name);
@@ -53,3 +53,53 @@ for (var i = 0; i < clubs.length; i++) {
 const categories = ['academic', 'culture-and-identity','environmental', 'general',
       'greek', 'visual-and-performance', 'political', 'professional', 
       'religious', 'service'];
+
+
+// Copied example from w3schools.com to filter lists
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
